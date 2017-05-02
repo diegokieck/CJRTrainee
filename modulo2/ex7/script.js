0b17d81979faa332af115de6247ae5bb4f13a8b5
DiegoKieck <diegokieck@gmail.com>
2017-05-02 04:15:27 -0300

    exercicio 7 feito

---
+++ modulo2/ex7/script.js
@@ -0,0 +1,15 @@
+function abreboca(){
+	
+	document.getElementById("openmouth").style.display = "initial";
+	document.getElementById('closedmouth').style.display = "block";
+}
+
+function fechaboca(){
+	
+	document.getElementById("openmouth").style.display = "none";
+	document.getElementById('closedmouth').style.display = "initial";
+}
+
+function mensagem(){
+	document.getElementById("mensagem").style.display = "initial";
+}
